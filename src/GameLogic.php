67f3ea941998319914c1c47db6e8af2a8296c171
julia <uliotapchenko@yandex.ru>
2023-10-11 20:18:38 +0300

    add step 7 and write logic for get nod

--- src/GameLogic.php
+++ src/GameLogic.php
@@ -37,13 +37,25 @@ function runGames(string $game_name)
                 line('Correct!');
             }
             break;
+        case 'BrainGcd':
+            line('Find the greatest common divisor of given numbers.');
+            for ($i = 0; $i < 3; $i++) {
+                [$answer, $right_answer] = BrainGcd();
+                if ($answer != $right_answer) {
+                    line("'$answer' is wrong answer ;(. Correct answer was '$right_answer'.");
+                    line("Let's try again, $name!");
+                    exit;
+                }
+                line('Correct!');
+            }
+            break;
     }
     line("Congratulations, $name!");
 }
 
 function BrainEven()
 {
-    $random_num = random_int(1, 100);
+    $random_num = getRandNum();
     line("Question: $random_num");
     $answer = prompt('Your answer');
     $right_answer = ($random_num % 2 == 0) ? 'yes' : "no";
@@ -52,13 +64,32 @@ function BrainEven()
 
 function BrainCalc()
 {
-    $random_num1 = random_int(1, 100);
-    $random_num2 = random_int(1, 100);
+    $random_num1 = getRandNum();
+    $random_num2 = getRandNum();
     $operation = getRandOperationForCalc();
     line("Question: $random_num1 $operation $random_num2");
     $answer = prompt('Your answer');
     $right_answer = eval('return ' . $random_num1 . $operation . $random_num2 . ';');
     return [$answer, $right_answer];
+}
+
+function BrainGcd()
+{
+    $random_num1 = getRandNum();
+    $random_num2 = getRandNum();
+    line("Question: $random_num1 $random_num2");
+    $answer = prompt('Your answer');
+    $right_answer = nod($random_num1,$random_num2);
+    return [$answer, $right_answer];
+}
+
+function nod($a, $b)
+{
+    if ($a % $b != 0) {
+        return nod($b, $a % $b);
+    } else {
+        return abs($b);
+    }
 }
 
 function getRandNum(): int
