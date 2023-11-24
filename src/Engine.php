9f8f41bb432b35aaee5500751b84e6e091a0c3cb
julia <uliotapchenko@yandex.ru>
2023-11-24 23:24:57 +0300

    fix 3

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,7 @@ namespace src\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function runGames(array $getQuestionAndAnswer,string $rules)
+function runGames($getQuestionAndAnswer,string $rules)
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
